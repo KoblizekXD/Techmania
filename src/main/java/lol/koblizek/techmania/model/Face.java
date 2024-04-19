f10514a108c6c8754007ec78ab5624aacaafc34e
KoblizekXD <janprokupek@centrum.cz>
2024-04-19 07:25:00 +0000

    uwu

--- src/main/java/lol/koblizek/techmania/model/Face.java
+++ src/main/java/lol/koblizek/techmania/model/Face.java
@@ -4,7 +4,6 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gl.ShaderProgram;
 import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.Identifier;
 import org.lwjgl.opengl.GL11;
 
 import java.util.ArrayList;
